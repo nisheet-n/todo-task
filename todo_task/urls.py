422fa1ee1070e0831c9ceb5dadc71ffd984c337c
Nisheet <nisheet2000@gmail.com>
2021-05-16 01:10:44 +0530

    Authentication System Done

--- todo_task/urls.py
+++ todo_task/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Auth
+    path('signup/', views.signupuser, name='signupuser'),
+    path('login/', views.loginuser, name='loginuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+
+    # ToDo
+    path('', views.home, name='home'),
+    path('current/', views.currenttodo, name='currenttodo'),
 ]
